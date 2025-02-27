cfbee09b907fc4867cb0ab8320db70b6fb02721c
amineproject <aminedatabases@gmail.com>
2025-02-27 11:28:41 +0100

    init microservices

---
+++ email-service/src/test/java/net/amine/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.amine.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
